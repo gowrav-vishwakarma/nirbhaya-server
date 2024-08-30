8e19250f0f3d53a27e465972f5b13e66c98f060a
saddam chouhan <saddam.c@frendy.in>
2024-08-30 16:42:17 +0530

    add db connection

--- src/config/config.js
+++ src/config/config.js
@@ -2,38 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: '',
-    password: '',
-    host: '',
-    database: 'process.env.DB_DATABASE',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     dialect: 'mysql',
-
     logQueryParameters: true,
     dialectOptions: {
-      // "useUTC": false, //for reading from database
       dateStrings: true,
       typeCast: true,
       connectTimeout: 3000,
     },
-    // timezone: '+05: 30',
-    // define: {
-    // hooks: {
-    // beforeBulkUpdate: (m) => {
-    //   let conditions = JSON.parse(JSON.stringify(Object.keys(m.where)));
-    //   conditions = conditions.filter((i) => i != "status");
-    //   if (conditions.length == 0) {
-    //     throw new Error(
-    //       "Bulk update with no specific conditions, status is not considered"
-    //     );
-    //   }
-    // },
-    // beforeBulkDestroy: (m) => {
-    //   if (m.model.getTableName() != "homeShopCarts" || Object.keys(m.where).length == 0) {
-    //     throw new Error("Bulk Destroy is disabled");
-    //   }
-    // },
-    // },
-    // },
   },
 };
