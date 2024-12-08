e40cf6745630841ac43ea6482a39768caa94ca44
Gowrav Vishwakarma <gowravvishwkarma@gmail.com>
2024-12-08 11:25:45 +0530

    Implement version check feature in App module. Added a new POST endpoint in AppController to check the current app version and updated AppService to return version details, including latest version and update URLs for both Android and iOS.

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,15 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  checkVersion(currentVersion: string) {
+    return {
+      latestVersion: '0.0.185',
+      forceUpdate: false,
+      minimumVersion: '0.0.185',
+      androidUpdateUrl:
+        'https://play.google.com/store/apps/details?id=com.nirbhaya.app',
+      iosUpdateUrl: 'https://apps.apple.com/app/id6444352309',
+    };
+  }
 }
